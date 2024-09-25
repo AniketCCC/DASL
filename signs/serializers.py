0fb6b5d456a169fa783c5497deddc9d65dac91bf
Luke Ostrander <luke.ostrander19@gmail.com>
2024-09-25 01:14:17 -0400

    Added overall documentation to Signs backend

--- signs/serializers.py
+++ signs/serializers.py
@@ -1,3 +1,14 @@
+"""
+serializers.py
+This module defines the serializer for the Sign model using Django REST framework's ModelSerializer.
+Serializers in Django REST framework are used to convert complex data types, 
+    such as querysets and model instances, into native Python datatypes that can then be 
+    easily rendered into JSON, XML, or other content types.
+They also provide deserialization, allowing parsed data to be converted back into complex types, 
+    after first validating the incoming data.
+Classes:
+    SignSerializer: A serializer for the Sign model that includes all fields of the model.
+"""
 from rest_framework import serializers
 from signs.models import Sign
 
