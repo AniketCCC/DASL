a093830bc1f5550c753c1ec1283375f5ce5d647a
Aniket <aniket.chatterjee@gmail.com>
2024-10-23 05:57:25 -0400

    Login and registration prelim implementation

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import react, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom'
 import back_icon from '../assets/arrow-back.svg';
+import Cookies from "universal-cookie";
 
 
 
@@ -10,34 +11,53 @@ export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    async function Login(e) {
+    async function login(e) {
         e.preventDefault();
-        
-    }
+				fetch("http://localhost:8000/api/login/", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					credentials: "include",
+					body: JSON.stringify({username: email, password: password}),
+				}).then(response => {
+      	if (!response.ok) {
+        	throw new Error('Network response was not ok');
+      	}
+      		return response.json();
+    		})
+				//.then(this.isResponseOk)
+				.then((data) => {
+					console.log(data);
+					//this.setState({isAuthenticated: true, username: "", password: "", error: ""});
+				})
+				.catch((err) => {
+					console.log(err);
+					//this.setState({error: "Wrong username or password."});
+				});
+  	}   
 
     return (
-        <html>
-            <body>
+								<div>
                 <div className="flex flex-col min-w-screen min-h-screen items-center align-middle justify-center container-snap">
                     <h1 className="flex flex-row justify-center text-center text-6xl mb-20">Login</h1>
 
-                    <form className="flex flex-col items-center justify-center align-middle">
-                        <input name="Email" placeholder="Email" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3"/>
-                        <input name="Password" placeholder="Password" type="password" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3"/>
+                    <form className="flex flex-col items-center justify-center align-middle" onSubmit={login}>
+                        <input name="Email" placeholder="Email" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3" value={email} onChange = {e => setEmail(e.target.value)}/>
+                        <input name="Password" placeholder="Password" type="password" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3" value={password} onChange = {e => setPassword(e.target.value)}/>
                     
-                        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center me-2 mt-20 mb-2">Login</button>    
+                        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center me-2 mt-20 mb-2">Login</button>    
                     </form>
                     <h3 className="m-4 ">Don't have an account? <Link to="/register" className="text-blue-300 hover:text-blue-500 focus:text-blue-700">Sign Up Here.</Link></h3>
                 </div>
 
-                <div class="fixed bottom-0 right-0 p-20">
+                <div className="fixed bottom-0 right-0 p-20">
                     <Link to="/">
-                        <button class="w-24 h-24 rounded-lg flex justify-center items-center border border-black-200 shadow-lg hover:bg-slate-200">
+                        <button className="w-24 h-24 rounded-lg flex justify-center items-center border border-black-200 shadow-lg hover:bg-slate-200">
                             <img src={back_icon} alt="Back"/>
                         </button>
                     </Link>
                 </div>
-            </body>
-        </html>
+								</div>
     );
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -11,9 +11,8 @@ export default function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    async function Signup(e) {
+    async function register(e) {
         e.preventDefault();
-
         //data to be sent in POST
         const postData = {
             'username': name,
@@ -22,11 +21,12 @@ export default function Register() {
         };
 
         //fetch request
-        fetch('http://localhost:8000/api/signup', {
+        fetch('http://localhost:8000/api/register/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+						credentials: "include",
             body: JSON.stringify(postData)
         }).then(response => {
             if(!response.ok) {
@@ -43,28 +43,28 @@ export default function Register() {
     }
 
     return (
-        <html>
-            <body>
+        <div>
+            <div>
                 <div className="flex flex-col min-w-screen min-h-screen items-center align-middle justify-center container-snap">
                     <h1 className="flex flex-row justify-center text-center text-6xl mb-20">Sign Up</h1>
 
-                    <form className="flex flex-col items-center justify-center align-middle">
-                        <input name="Name" placeholder="Name" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3"/>
-                        <input name="Email" placeholder="Email" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3"/>
-                        <input name="Password" placeholder="Password" type="password" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3"/>
+                    <form className="flex flex-col items-center justify-center align-middle" onSubmit={register}>
+                        <input name="Name" placeholder="Name" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3" value={name} onChange={e => setName(e.target.value)}/>
+                        <input name="Email" placeholder="Email" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3" value={email} onChange={e => setEmail(e.target.value)}/>
+                        <input name="Password" placeholder="Password" type="password" className="bg-gray-300 m-2 flex flex-col rounded-lg align-middle focus:ring-offset-white focus:border-offset-white justify-center w-80 h-12 p-3" value={password} onChange={e => setPassword(e.target.value)} />
 
-                        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center me-2 mt-20 mb-2">Sign Up</button>    
+                        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center me-2 mt-20 mb-2">Sign Up</button>    
                     </form>
                 </div>
 
-                <div class="fixed bottom-0 right-0 p-20">
+                <div className="fixed bottom-0 right-0 p-20">
                     <Link to="/">
-                        <button class="w-24 h-24 rounded-lg flex justify-center items-center border border-black-200 shadow-lg hover:bg-slate-200">
+                        <button className="w-24 h-24 rounded-lg flex justify-center items-center border border-black-200 shadow-lg hover:bg-slate-200">
                             <img src={back_icon} alt="Back"/>
                         </button>
                     </Link>
                 </div>
-            </body>
-            </html>
+            </div>
+            </div>
     );
 }
